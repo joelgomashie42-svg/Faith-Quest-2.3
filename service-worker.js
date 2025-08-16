90b7845d8fa06e930a296c29af3cb6d3d2e6f317
joelgomashie42-svg <joelgomashie42@gmail.com>
2025-08-16 08:57:49 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+﻿const CACHE = 'faith-quest-cache-v1';
+const ASSETS = [
+  'index.html','categories.html','books.html','levels.html','quiz.html','results.html','about.html',
+  'css/style.css','js/splash.js','js/quiz.js','questions.js','manifest.json'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e=> e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
